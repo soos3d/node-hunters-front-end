9cf44a5bd2b4a5334fbaa5272e2a977c5fcaec40
Soos3D <99700157+soos3d@users.noreply.github.com>
2023-02-16 13:10:07 -0300

    remake front end

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-import chainstackNh from "../src/images/chainstackNh.png";
-import logo from "../src/images/logo-full.svg";
+import logo from "../src/images/logo.png";
 
 const Header = ({ lastUpdate }) => {
   return (
-    <header className="header bg-gradient-to-l from-blue-600 via-blue-700 to-blue-900 text-white shadow-md p-5">
-      <div className="container mx-auto mb-5">
+    <header className="header bg-gradient-to-l from-purple-600 via-purple-700 to-purple-900 text-white shadow-md p-5">
+      <div className="container mx-auto">
         <div className="navbar flex items-center justify-between">
           <div className="logo flex items-center">
-          <a target="blank" href="https://chainstack.com">
             <img
               src={logo.src}
-              alt="Logo"
-              className="ml-5 w-1/2 h-auto object-scale-down"
+              alt="Nodes hunter logo"
+              className="ml-5 h-auto"
             />
-          </a>
+            <div className="container">
+              <p className="font-cursive text-2xl font-bold uppercase tracking-wider">
+                Nodes Hunter
+              </p>
+            </div>
           </div>
           <div className="links flex items-center space-x-5">
             <a
@@ -33,18 +35,20 @@ const Header = ({ lastUpdate }) => {
             >
               <i className="fab fa-github"></i> Backend
             </a>
+            <a
+              target="_blank"
+              href="https://twitter.com/web3Dav3"
+              className="twitter-button bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full"
+            >
+              <i className="fab fa-twitter"></i> Twitter
+            </a>
           </div>
         </div>
       </div>
-      <div class="logo relative pt-10 mb-10">
-        <p class="text-2xl text-blue-400 absolute w-full text-center">
-          Ethereum nodes performance with
+      <div class="logo relative pt-10 mb-20">
+        <p class="text-3xl absolute w-full text-center">
+          Explore Ethereum nodes providers
         </p>
-        <img
-          src={chainstackNh.src}
-          alt="Logo"
-          class="mx-auto z-0 w-1/2 h-auto object-scale-down mt-10"
-        />
       </div>
       <div className="flex items-center justify-center">
         <div className="bg-white rounded-md shadow-md p-4 text-black w-1/2">
@@ -64,10 +68,10 @@ const Header = ({ lastUpdate }) => {
               Send 11 requests in a row, remove the first one, and calculate the
               average of the ten remaining requests.
             </li>
-            <li>
-              The results are updated every 5 minutes.
-            </li>
-            <p class="mx-auto font-bold mt-5 font-lg">The first request is eliminated as it is often non-representative.</p>
+            <li>The results are updated every 5 minutes.</li>
+            <p class="mx-auto font-bold mt-5 font-lg">
+              The first request is eliminated as it is often non-representative.
+            </p>
           </ul>
         </div>
       </div>
